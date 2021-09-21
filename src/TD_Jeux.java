320531475adfbc2329040491ba720877452dfb9b
Logan De Jesus <logan.dejesus1@gmail.com>
2021-09-21 10:50:47 +0200

    oui

--- src/TD_Jeux.java
+++ src/TD_Jeux.java
@@ -1,7 +1,7 @@
 public class TD_Jeux {
 
     public static void main(String[] args) {
-        resoudre(100);
+        resoudre(20);
     }
 
     public static void resoudreAux(int n, int i, int j){
@@ -12,12 +12,34 @@ public class TD_Jeux {
 
         if(n>1){
             resoudreAux(n-1,i,k);
-            resoudreAux(1,i,j);
+            System.out.println(i+" -> "+j);
             resoudreAux(n-1,k,j);
         }
     }
 
     public static void resoudre(int n){
         resoudreAux(n,1,3);
+        System.out.println("Nombre de coup = " + countCoup(n,1,3,0));
+        System.out.println("Resultat de Un = 2^n -1 => "+(calculSuite(n)-1));
+    }
+    public static int countCoup(int n, int i, int j, int count){
+        int k = 6-i-j;
+
+        if(n==1){
+            return count+=1;
+        }
+
+        if(n>1){
+            count = countCoup(n-1,i,k,count);
+            count = countCoup(1,i,k,count);
+            count = countCoup(n-1,k,j,count);
+        }
+
+        return count;
+    }
+    public static int calculSuite(int n){
+        if(n>0)
+            return  (2 * calculSuite(n-1));
+        return 1;
     }
 }
